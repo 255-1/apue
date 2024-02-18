310edcc7bcf6d0f1012a11a578b19710c2ea039e
255-1 <1911646322@qq.com>
2024-02-18 16:52:43 +0800

    commit

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-rm -rf ./build; mkdir build;cd build; cmake .. -DCMAKE_BUILD_TYPE=Debug; cmake --build . --config Debug; cd ..;
+rm -rf ./build;
+mkdir build;
+cd build;
+cmake .. -DCMAKE_BUILD_TYPE=Debug; 
+cmake --build . --config Debug; 
+cd ..;
+programName=`tail -n1 CMakeLists.txt | grep -oP '(?<=\()\w+'`
+./build/$programName /home
